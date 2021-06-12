06ccd807f7c0c90711ac45ce253d78831d7d5c27
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-06-12 18:53:30 -0400

    add inquirer

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
