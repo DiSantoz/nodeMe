4b220b3fa816bedac331fc270cc2148f6c8d0be7
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-06-12 21:27:41 -0400

    add additional questions

--- index.js
+++ index.js
@@ -4,13 +4,40 @@ const inquirer = require('inquirer');
 const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
         {
         type: 'input',
-        name: 'name',
+        name: 'project',
         message: 'What is the name of your project? (Required)',
     },
-    ]
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What does your project do?',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How is your project installed?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is your project used?',
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'List GitHub usernames of anyone who contributed to this project',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'What is your GitHub username?'
+    },
+    ])
+}
 
 
     // TODO: Create a function to write README file
