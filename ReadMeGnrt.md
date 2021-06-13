dfc12876458f2ff24c5bbecf4125449980aad416
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-06-13 11:10:20 -0400

    display generated readme

---
+++ ReadMeGnrt.md
@@ -0,0 +1,32 @@
+# alpha
+
+  ![badmath](https://img.shields.io/badge/license-mit-green)
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  
+  #Description
+  no
+
+  #Installation
+  yes
+
+  #Usage
+  no
+
+  #License
+  # License 
+  https://choosealicense.com/licenses/mit
+  
+  
+  #Contributing
+  
+
+  #Questions
+  disantoz
+  https://github.com/DiSantoz/nodeMe
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,29 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (!data.license) {
-    data.license = [];
+  if (!license) {
+    license = [];
   }
-  return `https://img.shields.io/badge/license-${data.license}-green`
+  return `![badmath](https://img.shields.io/badge/license-${license}-green)`
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!data.license) {
-    data.license = [];
+  if (!license) {
+    license = [];
   }
-  return `https://choosealicense.com/licenses/${data.license}`
+  return `https://choosealicense.com/licenses/${license}`
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!data.license) {
-    data.license = [];
+  if (!license) {
+    license = [];
   }
   return `# License 
-  ${renderLicenseLink(data.license)}
+  ${renderLicenseLink(license)}
   `
 }
 
@@ -42,21 +42,27 @@ function generateMarkdown(data) {
   * [Questions](#questions)
   
   #Description
+
   ${data.description}
 
   #Installation
+
   ${data.install}
 
   #Usage
+
   ${data.usage}
 
   #License
-  ${renderLicenseLink(data.license)}
+
+  ${renderLicenseSection(data.license)}
   
   #Contributing
+
   ${data.contribute}
 
   #Questions
+  
   ${data.username}
   ${data.email}
 `;
